36a5199f9498a158a891494af198160b12084940
Santosh Mathew <smathew@captechventures.com>
2021-01-06 12:08:14 -0500

    Update test name

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders To Do App header', () => {
   render(<App />);
   const linkElement = screen.getByText(/To Do App/i);
   expect(linkElement).toBeInTheDocument();
